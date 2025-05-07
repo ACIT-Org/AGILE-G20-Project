3c4bf24a418c4f33f355b47be14c129383345fd7
xandersubs <alexander.subarsky@gmail.com>
2025-05-07 09:31:27 -0700

    fixed matchstats

--- models/matchstats.py
+++ models/matchstats.py
@@ -6,8 +6,8 @@ class PlayerStats(db.Model):
     __tablename__ = 'PlayerStats'
 
     pstat_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    player_id = db.Column(db.Integer, db.ForeignKey('player.id'))  # Adjust table name if different
-    match_id = db.Column(db.Integer, db.ForeignKey('match.id'))    # Adjust table name if different
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'))  # Adjust table name if different
+    match_id = db.Column(db.Integer, db.ForeignKey('matches.id'))    # Adjust table name if different
     kills = db.Column(db.Integer)
     deaths = db.Column(db.Integer)
     assists = db.Column(db.Integer)
