00d335f8ce73e459e1cfa1a27fa6e3c2a0dc647d
josh <thegoodboy2002@yahoo.com>
2025-05-07 09:46:38 -0700

    gitignore update

--- models/matches.py
+++ models/matches.py
@@ -1,6 +1,10 @@
 from sqlalchemy.orm import mapped_column, relationship
-from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy import Integer, String, ForeignKey, Boolean
 from db import db
+from datetime import datetime as dt
+from datetime import timedelta
+
+
 
 class Match(db.Model):
     __tablename__ = "matches"
@@ -9,9 +13,13 @@ class Match(db.Model):
     winner = mapped_column(String)
     play_date = mapped_column(db.DateTime, nullable=False, default=db.func.now())
     map = mapped_column(String)
+    completed = mapped_column(Boolean)
     
     team1_id = mapped_column(Integer, ForeignKey("teams.id"))
     team2_id = mapped_column(Integer, ForeignKey("teams.id"))
     #need the foreign_keys attribute since we're referencing the same table twice
     team1 = relationship("Team", foreign_keys=[team1_id], back_populates="as_team1_matches")
     team2 = relationship("Team", foreign_keys=[team2_id], back_populates="as_team2_matches")
+
+    def completed_check():
+        pass
