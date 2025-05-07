cbc3985bdde44c9f7b891b85933b8b67204186a1
xandersubs <alexander.subarsky@gmail.com>
2025-05-07 09:35:41 -0700

    changed the matchstats tabel to match the rest

--- models/matchstats.py
+++ models/matchstats.py
@@ -5,14 +5,14 @@ from db import db
 class PlayerStats(db.Model):
     __tablename__ = 'PlayerStats'
 
-    pstat_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    player_id = db.Column(db.Integer, db.ForeignKey('players.id'))  # Adjust table name if different
-    match_id = db.Column(db.Integer, db.ForeignKey('matches.id'))    # Adjust table name if different
-    kills = db.Column(db.Integer)
-    deaths = db.Column(db.Integer)
-    assists = db.Column(db.Integer)
-    damageDealt = db.Column(db.Integer)
-    damageBlocked = db.Column(db.Integer)
-    healing = db.Column(db.Integer)
-    accuracy = db.Column(db.Integer)
-    characterplayed = db.Column(db.Integer)
+    pstat_id = mapped_column(Integer, primary_key=True, nullable=False)
+    player_id = mapped_column(Integer, ForeignKey('players.id'))  # Adjust table name if different
+    match_id = mapped_column(Integer, ForeignKey('matches.id'))    # Adjust table name if different
+    kills = mapped_column(Integer)
+    deaths = mapped_column(Integer)
+    assists = mapped_column(Integer)
+    damageDealt = mapped_column(Integer)
+    damageBlocked = mapped_column(Integer)
+    healing = mapped_column(Integer)
+    accuracy = mapped_column(Integer)
+    characterplayed = mapped_column(String)
