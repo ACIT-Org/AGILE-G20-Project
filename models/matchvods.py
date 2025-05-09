13d6026529d11066fc7014e2438f8d4c00d08988
AidenDeveloper <aidennara@gmail.com>
2025-05-09 10:59:03 -0700

    videos

--- models/matchvods.py
+++ models/matchvods.py
@@ -2,7 +2,6 @@ from sqlalchemy.orm import mapped_column, relationship
 from sqlalchemy import DECIMAL, Integer, String, ForeignKey, Boolean
 from db import db
 
-
 class MatchVOD(db.Model):
     __tablename__ = "vods"
     id = mapped_column(Integer, primary_key=True)
