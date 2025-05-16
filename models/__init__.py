9fbea5a3d50912cf08b6752c19ab0453c36496d4
xandersubs <alexander.subarsky@gmail.com>
2025-05-15 23:55:34 -0700

    added a login feature and cleaned up a lot of commented out code

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["Match", "Player", "Team","PlayerStats","Maps","Characters","MatchVOD"]
+__all__ = ["Match", "Player", "Team","PlayerStats","Maps","Characters","MatchVOD","Admins"]
 from .matches import Match
 from .players import Player
 from .teams import Team
@@ -6,3 +6,4 @@ from .matchstats import PlayerStats
 from .maps import Maps
 from .characters import Characters
 from .matchvods import MatchVOD
+from .admins import Admins
